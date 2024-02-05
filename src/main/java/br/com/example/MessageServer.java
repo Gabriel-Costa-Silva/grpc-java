2407c8d28f5543ad39a4adacbc617b6bb32ee7e8
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2024-02-05 12:08:05 -0300

    Adcionando dependencias para utilização de serverReflection

--- src/main/java/br/com/example/MessageServer.java
+++ src/main/java/br/com/example/MessageServer.java
@@ -1,15 +1,22 @@
 package br.com.example;
 
+import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
 
 import java.io.IOException;
 import java.util.logging.Logger;
 
 public class MessageServer {
     public static void main(String [] args) {
+
+        BindableService prs = ProtoReflectionService.newInstance();
+  //      prs.bindService();
         Server server  = ServerBuilder
                 .forPort(9090)
+                .addService(prs)
                 .addService(new MessageImpl()).build();
     try{
         server.start();
