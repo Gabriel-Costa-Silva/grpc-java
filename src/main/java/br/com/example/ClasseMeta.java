6ec135496b493475a85a7a4b79cf325fa42e9bef
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2024-02-06 11:02:45 -0300

    Configurando métodos para execução no pipeline

--- src/main/java/br/com/example/ClasseMeta.java
+++ src/main/java/br/com/example/ClasseMeta.java
@@ -1,7 +1,10 @@
 package br.com.example;
 
+import com.google.api.client.util.Value;
+import org.apache.hop.core.HopEnvironment;
 import org.apache.hop.core.Result;
 import org.apache.hop.core.RowMetaAndData;
+import org.apache.hop.core.exception.HopException;
 import org.apache.hop.core.logging.HopLogStore;
 import org.apache.hop.core.logging.LogLevel;
 import org.apache.hop.core.logging.LoggingBuffer;
@@ -11,26 +14,19 @@ import org.apache.hop.core.variables.Variables;
 import org.apache.hop.metadata.serializer.json.JsonMetadataProvider;
 import org.apache.hop.pipeline.PipelineMeta;
 import org.apache.hop.pipeline.RowProducer;
+import org.apache.hop.pipeline.engine.IPipelineEngine;
+import org.apache.hop.pipeline.engine.PipelineEngineFactory;
 import org.apache.hop.pipeline.engines.local.LocalPipelineEngine;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ClasseMeta {
 
     //HopEnvironment.init();
-    public void start () throws Exception {
-        IVariables ivariables = Variables.getADefaultVariableSpace();
+    public void start (String conteudoMensagem) throws Exception {
 
-        JsonMetadataProvider jsonMetadataProvider = new JsonMetadataProvider();
-        jsonMetadataProvider.setBaseFolder("caminho do /metadata");
-
-        PipelineMeta pm = new PipelineMeta("caminho do arquivo.hpl",jsonMetadataProvider,ivariables);
-
-        LocalPipelineEngine localPipelineEngine = new LocalPipelineEngine(pm,ivariables,null);//investigar este parent
-
-        localPipelineEngine.setLogLevel(LogLevel.BASIC);
-        localPipelineEngine.prepareExecution();
-
+        LocalPipelineEngine localPipelineEngine = getLocalPipelineEngine();
 
         RowProducer rowProducer = localPipelineEngine.addRowProducer("Injector",0); //localiza o Injector dentro do arquivo .hpl
 
@@ -56,6 +52,34 @@ public class ClasseMeta {
         } );
         LoggingBuffer loggingBuffer = HopLogStore.getAppender();
         String log = loggingBuffer.getBuffer(localPipelineEngine.getLogChannelId(),false).toString();
+    }
+
+    @NotNull
+    private static LocalPipelineEngine getLocalPipelineEngine() throws HopException {
+        IVariables ivariables = Variables.getADefaultVariableSpace();
+        //caso venha a usar
+        //variables.setVariable("teste","teste");
+       // String metadataFolder = MessageServer.getProperties().getProperty("hop.metadata.folder").toString();
+
+        JsonMetadataProvider jsonMetadataProvider = new JsonMetadataProvider();
+        jsonMetadataProvider.setBaseFolder(MessageServer.getProperties().getProperty("hop.metadata.folder").toString());
+
+        PipelineMeta pm = new PipelineMeta(
+                ClasseMeta.class.getClassLoader().getResourceAsStream("upper-case.hpl")
+                ,jsonMetadataProvider,
+                ivariables);
+//        retirado de https://blog.csdn.net/qq_41482600/article/details/129751239
+//        IPipelineEngine pipelineEngine = PipelineEngineFactory.createPipelineEngine(
+//                ivariables,
+//                "local",
+//                jsonMetadataProvider,
+//                pm
+//        );
+        LocalPipelineEngine localPipelineEngine = new LocalPipelineEngine(pm,ivariables,null);//investigar este parent
+
+        localPipelineEngine.setLogLevel(LogLevel.BASIC);
+        localPipelineEngine.prepareExecution();
+        return localPipelineEngine;
     }
 
 }
--- src/main/java/br/com/example/MessageServer.java
+++ src/main/java/br/com/example/MessageServer.java
@@ -1,36 +1,51 @@
 package br.com.example;
 
+import com.google.protobuf.Message;
 import io.grpc.BindableService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
+import org.apache.hop.core.HopEnvironment;
+import org.apache.hop.core.exception.HopException;
 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class MessageServer {
+    private static Properties properties;
     public static void main(String [] args) {
+        Logger logger = Logger.getLogger(MessageServer.class.getName());
+        properties = new Properties();
 
-        BindableService prs = ProtoReflectionService.newInstance();
-  //      prs.bindService();
-        Server server  = ServerBuilder
+        try (InputStream input = MessageServer.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(input);
+
+            //instanciamento de propriedades
+            System.setProperty("HOP_PLUGIN_BASE_FOLDER",properties.getProperty("hop.plugin"));
+//            System.setProperty("HOP_PLUGIN_BASE_FOLDER",prop.getProperty("hop.plugin"));
+//            System.setProperty("HOP_PLUGIN_BASE_FOLDER",prop.getProperty("hop.plugin"));
+
+            HopEnvironment.init();
+
+
+
+            BindableService prs = ProtoReflectionService.newInstance();
+            Server server  = ServerBuilder
                 .forPort(9090)
                 .addService(prs)
                 .addService(new MessageImpl()).build();
-    try{
-        server.start();
-        server.awaitTermination();
-    }catch(IOException|InterruptedException e ){
-        Logger.getLogger("ERROS:"+e.getMessage());
-
+            server.start();
+            server.awaitTermination();
+        }catch(IOException|InterruptedException |HopException e ) {
+            logger.severe("ERROS:" + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
-
-
-
+    public static Properties getProperties(){
+        return properties;
     }
-
-
-
 
 }
